ffde9c57cff5f02e6d6d3369870e01f9390726a2
RyanNall <31323098+RyanNall@users.noreply.github.com>
2017-10-05 12:34:14 -0700

    Chapter 1 Lab 1 Complete

---
+++ Chapter 1/Count.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Count here.
+1.      Use // style comments to add a comment header at the top of the file that includes the name of the program, your name, and a brief description of what the program does, neatly formatted. Include a delimiter line (e.g., all stars) at the beginning and end of the header. 
+	Answer: Count.java, Ryan Nall, Counts from 1 to 5 in English, French, and Spanish
+2.      Add a comment before each println that indicates what language the next line is in. Experiment with leaving a blank line before each of these comment lines (in the program itself, not the output). Is the program easier to read with or without these blank lines?
+	Answer: Yes
+3.      Remove one of the slashes from one of your comment lines and recompile the program, so one of the comments starts with a single /. What error do you get? Put the slash back in.
+	Answer: Semi colon expected
+4.      Try putting a comment within a comment, so that a // appears after the initial // on a comment line. Does this cause problems?
+	Answer: No
+
+ */
+
+public class Count
+{
+	public static void main (String[] args)
+	{
+         //English
+  	 System.out.println ("one two three four five");
+  	 
+  	 //French
+  	 System.out.println ("un deux trois quatre cinq");
+  	 
+  	 //Spanish
+  	 System.out.println ("uno dos tres cuatro cinco");
+ 
+	}    
+}
---
+++ Chapter 1/Simple.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Simple here.
+ 1.	simple (Why do you even have to change the name of the file in this case?)
+	Answer: It is lowercase instead ofa Capital letter. Yes its legal. No its a bad choice because first letter in identifiers are usually capital.
+2.	SimpleProgram
+	Answer: Yes its legal. Yes its good.
+3.	1Simple
+	Answer: No its not legal. Identifiers can't begin with a digit.
+4.	_Simple_
+	Answer: Yes its legal. Not good, identifier may be confusing since it doesnt start with a capital letter
+5.	*Simple*
+	Answer: No its not legal. Character is not legal in identifiers.
+6.	$123_45
+	Answer: Yes its legal. Not good, not descriptive of program 
+7.	Simple!
+	Answer: No its not legal. Exclamation point not legal in identifiers.
+
+ */
+public class Simple
+{
+   public static void main (String[] args)
+	{
+ 
+  	System.out.println ("Java rocks!!");
+ 
+	}
+
+}
